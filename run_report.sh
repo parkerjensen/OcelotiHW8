d7d2e1430a0ef8d0672d5da5bce0235442d42be6
Steven R. Nye <stevennye@mail.weber.edu>
2016-12-03 15:31:16 -0700

    added usage and help functions to wrapper

--- run_report.sh
+++ run_report.sh
@@ -19,4 +19,26 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+#Declaration of usage function
+usageState()
+{
+	echo "Usage: ./run_report.sh -u <FTPuser> -p <FTPpw> -e <email> -f <begDate> -t <endDate>"
+	echo "All arguments are REQUIRED"
+}
 
+#Declaration of help function
+helpFun()
+{
+	if [[ "$1" == "--help" ]]
+	then
+		echo "Usage: ./run_report.sh -u <FTPuser> -p <FTPpw> -e <email> -f <begDate> -t <endDate>"
+		echo "--help Print this help message"
+		echo "-u specifies the FTP user"
+		echo "-p specifies the FTP user's password"
+		echo "-e specifies the user's email"
+		echo "-f specifies the beginning date"
+		echo "-t specifies the end date"
+		echo "With no arguments it provides a usage statement"
+		exit
+	fi
+}
