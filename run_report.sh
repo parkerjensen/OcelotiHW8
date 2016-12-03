0abb83c59d630fd57ff89dca8773d3feb0d01764
Steven R. Nye <stevennye@mail.weber.edu>
2016-12-03 15:03:24 -0700

    added run_report.sh

---
+++ run_report.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: run_report.sh
+# 
+#         USAGE: ./run_report.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Steven R. Nye (), stevennye@mail.weber.edu
+#  ORGANIZATION: Weber State University
+#       CREATED: 12/03/2016 15:02
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
